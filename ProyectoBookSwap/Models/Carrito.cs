7851831f157451ffe89d1cb83ef13b220a770f10
Dairymateo <seoxyt15@gmail.com>
2024-05-08 12:26:07 -0400

    Agregar modelo carrito

---
+++ ProyectoBookSwap/Models/Carrito.cs
@@ -0,0 +1,9 @@
+﻿namespace ProyectoBookSwap.Models
+{
+    public class Carrito
+    {
+        public int IdCarrito { get; set; }
+        public Libros oProducto { get; set; }
+        public Usuarios oUsuario { get; set; }
+    }
+}
